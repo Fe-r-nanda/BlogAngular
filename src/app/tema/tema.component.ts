012b64ab05be288a5a60d290e0b35abd9ebb1adc
f-er-nanda <fefa.correia223@gmail.com>
2021-09-02 11:23:57 -0300

    Get e Post de Temas

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -9,13 +11,40 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
-  constructor(private router: Router) { }
+/*vamos instanciar, criar um obj para ser lido pelo ngmodeules no html (como se fosse leia no back)*/
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+
+  constructor(             /*no constructor é onde criamos as dependencias*/
+    private router: Router,
+    private temaService: TemaService
+    ) { }
+
+    /*aqui criamos os métodos que chamamos no html */
 
   ngOnInit() {
     if(environment.token == ''){
       alert('Sua seção expirou, faça o login novamente.')
       this.router.navigate(['/entrar'])
+
+
+      this.findAllTemas()   /*ou seja, td vez que abrir a pag de temas ele automaticamente traz tds ja*/
     }
+  }
+
+  findAllTemas(){                   /*o subscribe tranforma o json em typescript*/
+    this.temaService.getAllTema().subscribe((resp: Tema[])=>{
+      this.listaTemas = resp
+    })
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{this.tema = resp
+    alert ('Tema cadastrado com sucesso!')
+    this.findAllTemas()
+    this.tema = new Tema()
+  })
   }
 
 }
